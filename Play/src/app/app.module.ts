c72cc56e19ee17e79f71533f48dbc90686ad9520
ManuelJPastor <mjpastor2@alu.ucam.edu>
2020-04-07 11:20:02 +0200

    Ordenación 2

--- Play/src/app/app.module.ts
+++ Play/src/app/app.module.ts
@@ -5,12 +5,10 @@ import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-import { PreguntasComponent } from './preguntas/preguntas.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { Credenciales } from './credenciales';
-import { PreguntasService } from './preguntas/preguntas.service';
-import { Preguntas } from './preguntas/preguntas';
 import { ExplicacionComponent } from './components/explicacion/explicacion.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { PreguntasComponent } from './components/preguntas/preguntas.component';
+import { PreguntasService } from './services/preguntas.service';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
@@ -31,7 +29,7 @@ const routes: Routes = [
     FormsModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [PreguntasService, Credenciales, Preguntas],
+  providers: [PreguntasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
