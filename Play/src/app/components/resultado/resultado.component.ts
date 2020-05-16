5c474f3358f6890abd75c74aac1f3cb9c6b1b93c
ManuelJPastor <mjpastor2@alu.ucam.edu>
2020-05-16 20:22:04 +0200

    Send points

--- Play/src/app/components/resultado/resultado.component.ts
+++ Play/src/app/components/resultado/resultado.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PreguntasService } from 'src/app/services/preguntas.service';
 import { Puntos } from 'src/app/objects/puntos';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-resultado',
@@ -29,8 +30,15 @@ export class ResultadoComponent implements OnInit {
       this.frase = JSON.parse(localStorage['frases']) [2];
       document.getElementById('frase').className += " text-success";
     }
+
+    let percent:number = (this.puntuacion * 100) / this.puntuacionMax
+    this.puntos.percent = percent;
     this.preguntasService.enviarPuntos(this.puntos).subscribe(response => {
-      
+      if(response!.Message){
+        Swal.fire('Envío de puntuación', 'Lo sentimos, esta partida no cuenta', 'warning')
+      } else{
+        Swal.fire('Envío de puntuación', 'Se ha obtenido '+response!.action+' puntos. Ranking del juego: '+response!.match_ranking+'º posición.\n Puntuación del juego: '+response!.match_score+' puntos.' , 'success')
+      }
     })
 
   }
--- Play/src/app/objects/puntos.ts
+++ Play/src/app/objects/puntos.ts
@@ -1,8 +1,8 @@
 export class Puntos {
   validation: string;
   invitation: string;
-  percent: number = 100;
-  title: "¡¡Felicidades!!";
-  resume: "Has ganado puntos por jugar";
-  message: "Como has jugado al juego del QQSC has recibido puntos por ello";
+  percent: number;
+  title: string = "¡¡Felicidades!!";
+  resume: string = "Has ganado puntos por jugar";
+  message: string = "Como has jugado al juego del QQSC has recibido puntos por ello";
 }
